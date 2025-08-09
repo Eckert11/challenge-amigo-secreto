efdee18d8cffba296b117b848e72a976b795c546
Eckert11 <eckert.baezag@gmail.com>
2025-08-09 01:21:27 -0600

    Usando método push se actualiza el array de amigos.

--- app.js
+++ app.js
@@ -6,16 +6,20 @@ let amigos = []
 // Función para agregar amigos : Permitirà al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos del arreglo.
 function Agregaramigo() {
 
-    //Capturar el valor de entrada : Usar document.getElementById o document.queryselector para obtener el texto ingresado por el usuario.
+    // Capturar el valor de entrada : Usar document.getElementById o document.queryselector para obtener el texto ingresado por el usuario.
     const input = document.getElementById('amigo');
 
     // Obtener texto sin espacios al inicio o al final
     const amigo = input.value.trim();
 
-    //Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacio. Si está vacio, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre"
+    // Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacio. Si está vacio, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre"
     if (amigo === '') {
         alert('Por favor, inserte un nombre');
         return;
     }
+    amigos.push(amigo); // Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombres de amigos usando el método .push()
+
+
+
 
 }
