b61a7c20374668985ce8eb960514c11d84997084
Eckert11 <eckert.baezag@gmail.com>
2025-08-09 01:52:01 -0600

    Se Agrega funciòn para validar entrada y limpiar campo tras añadir nombre

--- app.js
+++ app.js
@@ -3,12 +3,16 @@
 // Variable de tipo array, que almacenará los nombres de los amigos ingresados.
 let amigos = []
 
+// Función para limpiar la caja.
+    function limpiarCampo() {
+        const input = document. getElementById('amigo');
+        input.value = '';
+    }
+
 // Función para agregar amigos : Permitirà al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos del arreglo.
 function Agregaramigo() {
-
     // Capturar el valor de entrada : Usar document.getElementById o document.queryselector para obtener el texto ingresado por el usuario.
     const input = document.getElementById('amigo');
-
     // Obtener texto sin espacios al inicio o al final
     const amigo = input.value.trim();
 
@@ -17,9 +21,10 @@ function Agregaramigo() {
         alert('Por favor, inserte un nombre');
         return;
     }
+
     amigos.push(amigo); // Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombres de amigos usando el método .push()
-
-
+    // Limpiar el campo de entrada: Despuès de añadir el nombre, restablecer el campo de texto a una cadena vacia.
+    limpiarCampo();
 
 
 }
